e241c29cd11a6775ec6b8401dc5582131a28128c
avidunixuser <avidunixuser@users.noreply.github.com>
2025-04-08 22:15:59 +0000

    Add endpoints to retrieve hotels and bookings for improved data access

--- src/ContosoSuitesWebAPI/Program.cs
+++ src/ContosoSuitesWebAPI/Program.cs
@@ -137,4 +137,31 @@ app.MapPost("/MaintenanceCopilotChat", async ([FromBody]string message, [FromSer
     .WithName("Copilot")
     .WithOpenApi();
 
+// Retrieve the set of hotels from the database.
+app.MapGet("/Hotels", async () => 
+{
+    var hotels = await app.Services.GetRequiredService<IDatabaseService>().GetHotels();
+    return hotels;
+})
+    .WithName("GetHotels")
+    .WithOpenApi();
+
+// Retrieve the bookings for a specific hotel.
+app.MapGet("/Hotels/{hotelId}/Bookings/", async (int hotelId) => 
+{
+    var bookings = await app.Services.GetRequiredService<IDatabaseService>().GetBookingsForHotel(hotelId);
+    return bookings;
+})
+    .WithName("GetBookingsForHotel")
+    .WithOpenApi();
+
+// Retrieve the bookings for a specific hotel that are after a specified date.
+app.MapGet("/Hotels/{hotelId}/Bookings/{min_date}", async (int hotelId, DateTime min_date) => 
+{
+    var bookings = await app.Services.GetRequiredService<IDatabaseService>().GetBookingsByHotelAndMinimumDate(hotelId, min_date);
+    return bookings;
+})
+    .WithName("GetRecentBookingsForHotel")
+    .WithOpenApi();
+
 app.Run();
